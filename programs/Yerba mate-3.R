39fa71e696de51eb2aeb773925d932c50e4dad2b
Abigail Riquelme <82173618+AbigailRiquelme@users.noreply.github.com>
2024-09-27 10:46:15 -0300

    Add files via upload

---
+++ programs/Yerba mate-3.R
@@ -0,0 +1,251 @@
+#---------------------------------------------------------------#
+#                       Yerba mate
+#     Ministerio de Desregulación y Transformación del Estado
+#           Secretaria de Simplificación del Estado
+#                       Martín Rossi
+#     Autores: Abigail Riquelme y Facundo Gómez García
+#---------------------------------------------------------------#
+
+#install.packages("showtext")
+library(readxl)
+library(lubridate)
+library(ggplot2)
+library(ggtext)
+library(dplyr)
+library(scales)
+library(showtext)
+library(writexl)
+
+font_add_google("Montserrat", "montserrat")
+showtext_auto()
+
+setwd("/Users/ariquelme/Downloads")
+
+yerba_precio <- read_excel("IPCBA_base_2021100-Precios_medios_alim.xlsx", sheet = "Yerba mate")
+yerba_precio <- yerba_precio %>%
+  rename(Periodo = "Descripción")
+yerba_precio$Periodo <- as.Date(yerba_precio$Periodo, format = "%Y-%m-%d")
+names(yerba_precio) <- gsub(" ", "_", names(yerba_precio))
+
+yerba_precio$Ponderar <- yerba_precio$Ponderar/100
+
+#indico el año de corte que divide las tendencias en dos. Previo y posterior a la asunción de milei.
+cutoff <- as.Date("2023-12-10")
+
+
+#Graficamos:
+ggplot(yerba_precio, aes(x = Periodo, y = Yerba_mate)) + 
+  geom_line(color = "#46658B", size = 1) +
+  geom_segment(aes(x = as.Date("2024-1-01"), xend = as.Date("2024-1-01"), 
+                   y = 0, yend = 4500), 
+               linetype = "dashed", color = "#E6B861", linewidth = 1) +
+  geom_label(aes(x = as.Date("2023-09-01"), y = 3800, 
+                 label = "DNU 70/2023"),
+             color = "black", fill = "white", size = 3.5, 
+             family = "montserrat", label.size = 0.5, label.padding = unit(0.25, "lines")) +
+  scale_x_date(date_labels = "%Y-%m", date_breaks = "6 months") +
+  theme(legend.position = "none", 
+        panel.background = element_rect(fill = 'transparent'), 
+        plot.background = element_rect(fill = 'transparent', color = NA), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        legend.background = element_rect(fill = 'transparent'),
+        legend.box.background = element_rect(fill = 'transparent'),
+        text = element_text(size = 14, family = "montserrat"),
+        axis.title.x = element_text(margin = margin(t = 10)), 
+        axis.title.y = element_text(margin = margin(r = 10)),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10),
+        plot.margin = margin(t = 5, r = 20, b = 5, l = 10)) + 
+  labs(title = "Evolución del precio medio de la yerba mate",
+       caption = "Fuente: Elaboración propia en base a datos del IPCBA") +
+  ylab("Pesos") +
+  xlab("Mes")
+
+
+#### Gráfico variación ####
+
+#Calculamos la variación porcentual mes a mes
+yerba_precio <- yerba_precio %>%
+  arrange(Periodo) %>% 
+  mutate(variacion_mensual = (Yerba_mate - lag(Yerba_mate)) / lag(Yerba_mate) * 100)
+
+ggplot(yerba_precio, aes(x = Periodo, y = variacion_mensual)) + 
+  geom_line(color = "#46658B", size = 1) +
+  geom_segment(aes(x = as.Date("2024-1-01"), xend = as.Date("2024-1-01"),                   y = -4, 
+                   yend = 30), 
+               linetype = "dashed", color = "#E6B861", linewidth = 1) +
+  
+  geom_label(aes(x = as.Date("2024-04-01"), 
+                 y = max(variacion_mensual, na.rm = TRUE) * 0.95, 
+                 label = "DNU 70/2023"),
+             color = "black", fill = "white", size = 3.5, 
+             family = "montserrat", label.size = 0.5, label.padding = unit(0.25, "lines")) +
+  
+  geom_hline(yintercept = 0, color = "#4D4D4D", linetype = "dashed", size = 0.8) +
+  scale_x_date(date_labels = "%Y-%m", date_breaks = "3 months") +
+  theme(legend.position = "none", 
+        panel.background = element_rect(fill = 'transparent'), 
+        plot.background = element_rect(fill = 'transparent', color = NA), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        legend.background = element_rect(fill = 'transparent'),
+        legend.box.background = element_rect(fill = 'transparent'),
+        text = element_text(size = 14, family = "montserrat"),
+        axis.title.x = element_text(margin = margin(t = 10)), 
+        axis.title.y = element_text(margin = margin(r = 10)),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10),
+        plot.margin = margin(t = 5, r = 20, b = 5, l = 10)) + 
+  labs(title = "Variación porcentual mensual del precio de la yerba mate",
+       caption = "Las variaciones porcentuales se calculan mes a mes \n Fuente: Elaboración propia en base a datos del IPCBA") +
+  ylab("Variación porcentual (%)") +
+  xlab("Mes") +
+  scale_y_continuous(limits = c(-10, 30), # Ajustamos los límites para ver la línea segmentada
+                     breaks = seq(-10, 30, by = 5))
+
+#### Gráfico precios constantes ####
+yerba_precio$yerba_real <- yerba_precio$Yerba_mate/yerba_precio$Ponderar
+
+((yerba_precio$yerba_real[30]-yerba_precio$yerba_real[22])/yerba_precio$yerba_real[22])*100
+    
+ggplot(yerba_precio, aes(x = Periodo, y = yerba_real)) + 
+  geom_line(color = "#46658B", size = 1) +
+  geom_segment(aes(x = as.Date("2024-1-01"), xend = as.Date("2024-1-01"),                   y = 350, yend = 600), 
+               linetype = "dashed", color = "#E6B861", linewidth = 1) +
+  geom_label(aes(x = as.Date("2023-10-01"), y = 550, 
+                 label = "DNU 70/2023"),
+             color = "black", fill = "white", size = 4.5, 
+             family = "montserrat", label.size = 0.5, label.padding = unit(0.25, "lines")) +
+  scale_x_date(date_labels = "%Y-%m", date_breaks = "2 months") +
+  theme(legend.position = "none", 
+        panel.background = element_rect(fill = 'transparent'), 
+        plot.background = element_rect(fill = 'transparent', color = NA), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        legend.background = element_rect(fill = 'transparent'),
+        legend.box.background = element_rect(fill = 'transparent'),
+        text = element_text(size = 14, family = "montserrat"),
+        axis.title.x = element_text(margin = margin(t = 10)), 
+        axis.title.y = element_text(margin = margin(r = 10)),
+        axis.text.x = element_text(size = 10, angle=45),
+        axis.text.y = element_text(size = 10),
+        plot.margin = margin(t = 5, r = 30, b = 5, l = 10)) + 
+  labs(title = "Evolución del precio promedio a pesos constantes del kilo de yerba mate",
+       caption = "Fuente: Elaboración propia en base a datos del IPCBA") +
+  ylab("Pesos") +
+  xlab("Mes") +
+  geom_label(aes(x = as.Date("2024-06-01"), y = 500, 
+                 label = "2024-08 vs. 2023-12\n-25.4%"),
+             color = "black", fill = "white", size = 4.5, 
+             family = "montserrat", label.size = 0.5, label.padding = unit(0.25, "lines")) +
+  annotate(
+    geom = "curve", x = as.Date("2024-06-01"), xend = as.Date("2024-08-01"), y = 487, yend = 400.67, curvature = 0, 
+    angle = 60, color = "#E6B861", linewidth = .4, 
+    arrow = arrow(type = "closed", length = unit(.08, "inches")))
+
+
+yerba_precio_1 <- yerba_precio[yerba_precio$Periodo>="2023-01-01",]
+
+ggplot(yerba_precio_1, aes(x = Periodo, y = yerba_real)) + 
+  geom_line(color = "#46658B", size = 1) +
+  geom_segment(aes(x = as.Date("2023-12-01"), xend = as.Date("2023-12-01"),                   y = 350, yend = 600), 
+               linetype = "dashed", color = "#E6B861", linewidth = 1) +
+  geom_label(aes(x = as.Date("2023-10-01"), y = 550, 
+                 label = "DNU 70/2023"),
+             color = "black", fill = "white", size = 4.5, 
+             family = "montserrat", label.size = 0.5, label.padding = unit(0.25, "lines")) +
+  scale_x_date(date_labels = "%Y-%m", date_breaks = "2 months") +
+  theme(legend.position = "none", 
+        panel.background = element_rect(fill = 'transparent'), 
+        plot.background = element_rect(fill = 'transparent', color = NA), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        legend.background = element_rect(fill = 'transparent'),
+        legend.box.background = element_rect(fill = 'transparent'),
+        text = element_text(size = 14, family = "montserrat"),
+        axis.title.x = element_text(margin = margin(t = 10)), 
+        axis.title.y = element_text(margin = margin(r = 10)),
+        axis.text.x = element_text(size = 10, angle=45),
+        axis.text.y = element_text(size = 10),
+        plot.margin = margin(t = 5, r = 30, b = 5, l = 10)) + 
+  labs(title = "Evolución del precio promedio a pesos constantes del kilo de yerba mate",
+       caption = "Fuente: Elaboración propia en base a datos del IPCBA") +
+  ylab("Pesos") +
+  xlab("Mes") +
+  geom_label(aes(x = as.Date("2024-06-01"), y = 500, 
+                 label = "2024-08 vs. 2023-12\n-25.4%"),
+             color = "black", fill = "white", size = 4.5, 
+             family = "montserrat", label.size = 0.5, label.padding = unit(0.25, "lines")) +
+  annotate(
+    geom = "curve", x = as.Date("2024-06-01"), xend = as.Date("2024-08-01"), y = 487, yend = 400.67, curvature = 0, 
+    angle = 60, color = "#E6B861", linewidth = .4, 
+    arrow = arrow(type = "closed", length = unit(.08, "inches")))
+
+
+
+
+
+
+#### Gráfico precios constantes sin estacionalidad ####
+
+
+yerba_precio$mes <- format(yerba_precio$Periodo, "%m")
+yerba_precio$mes <- factor(yerba_precio$mes, levels = sprintf("%02d", 1:12))
+yerba_precio <- yerba_precio %>%
+  arrange(mes)
+
+modelo <- lm(yerba_real ~ factor(mes), data = yerba_precio)
+summary(modelo)
+yerba_precio$residuos <- residuals(modelo)
+
+yerba_precio$residuos_normalizados <- scale(yerba_precio$residuos)
+
+
+ggplot(yerba_precio, aes(x = Periodo, y = residuos)) + 
+  geom_line(color = "#46658B", size = 1) +
+  geom_segment(aes(x = as.Date("2024-01-01"), xend = as.Date("2024-1-01"), y = -100, yend = 100), 
+               linetype = "dashed", color = "#E6B861", linewidth = 1) +
+  geom_label(aes(x = as.Date("2023-11-01"), y = 65, 
+                 label = "DNU 70/2023"),
+             color = "black", fill = "white", size = 4.5, 
+             family = "montserrat", label.size = 0.5, label.padding = unit(0.25, "lines")) +
+  scale_x_date(date_labels = "%Y-%m", date_breaks = "6 months") +
+  theme(legend.position = "none", 
+        panel.background = element_rect(fill = 'transparent'), 
+        plot.background = element_rect(fill = 'transparent', color = NA), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        legend.background = element_rect(fill = 'transparent'),
+        legend.box.background = element_rect(fill = 'transparent'),
+        text = element_text(size = 14, family = "montserrat"),
+        axis.title.x = element_text(margin = margin(t = 10)), 
+        axis.title.y = element_text(margin = margin(r = 10)),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10),
+        plot.margin = margin(t = 5, r = 20, b = 5, l = 10)) + 
+  labs(title = "Evolución del precio medio a pesos constantes de la yerba mate sin estacionalidad",
+       caption = "Fuente: Elaboración propia en base a datos del IPCBA") +
+  ylab("Pesos") +
+  xlab("Mes") + 
+  geom_label(aes(x = as.Date("2024-06-01"), y = 38, 
+                 label = "2024-08 vs. 2023-12\n-25.4%"),
+             color = "black", fill = "white", size = 4.5, 
+             family = "montserrat", label.size = 0.5, label.padding = unit(0.25, "lines")) +
+  annotate(
+    geom = "curve", x = as.Date("2024-06-01"), xend = as.Date("2024-08-01"), y = 30, yend = -40, curvature = 0, 
+    angle = 60, color = "#E6B861", linewidth = .4, 
+    arrow = arrow(type = "closed", length = unit(.08, "inches")))
+
+
+prueba <- yerba_precio[yerba_precio$Periodo%in%c("2023-12-01","2024-08-01"),]
+
+
+#Guardamos la base de datos para las estimaciones:
+write_xlsx(yerba_precio, "C:/Users/Usuario/Desktop/Trabajo - UdeSA/Trabajo - Ministerio/Yerba/input/yerba_precio_est.xlsx")
+
+
+
+
+
+
---
+++ programs/Yerba_Estimaciones.do
@@ -0,0 +1,93 @@
+/*******************************************************************************
+								Yerba (DNU 70/2023)
+			  Ministerio de Desregulación y Transformación del Estado
+					 Secretaria de Simplificación del Estado
+								 Martín Rossi
+				Autores: Abigail Riquelme y Facundo Gómez García
+********************************************************************************/
+clear all
+set more off
+
+else if  "`c(username)'" == "Usuario" {
+	global main "direccion"
+	}
+
+*Crear carpetas de "input" (donde estará la base de datos) y "output" (para las tablas/gráficos):
+global input "$main/input"
+global output "$main/output"
+
+cd "$main"
+
+import excel "$input/yerba_precio_est.xlsx", firstrow clear
+drop mes
+
+*===========================*
+*		ESTIMACIONES
+*===========================*
+*Se generaron dos variables clave para el análisis: una que captura la tendencia temporal durante el periodo estudiado, y otra que identifica los meses afectados por la modificación del DNU. Los meses tratados son aquellos posteriores a la fecha de la modificación/derogación, por lo que se consideran como tratados las observaciones luego de diciembre de 2023. Además, para minimizar el ruido en los datos, restringimos el análisis a un periodo cercano a la intervención, limitándolo a las variaciones observadas entre marzo de 2022 y la actualidad.
+sort Periodo
+gen treat=0
+replace treat=1 if Periodo>mdy(12,1,2023)
+gen t=_n
+
+*Agregamos un control más a la estimación que se define como la interacción entre la variable de tratamiento y la tendencia:
+gen treat_t=treat*t
+
+*A continuación, se presentan las estimaciones utilizando un modelo "Before and After" con tendencia temporal. En todas las especificaciones, la variable de interés es la interacción entre el tratamiento y la tendencia temporal, es decir, Tratamiento*Tendencia. El coeficiente asociado a esta variable capturará el cambio en la tendencia tras la modificación implementada a partir del DNU, permitiendo determinar si la intervención generó en promedio una disminución o un aumento en el precio de la Yerba Mate a precios constantes.
+*De acuerdo con los resultados, el coeficiente de la interacción es negativo y significativo, lo que sugiere que, tras la intervención, el precio del kilo de yerba a precios constantes comenzó a disminuir a lo largo del tiempo.
+ 
+label variable t "Tendencia"
+label variable treat "Tratamiento"
+label variable treat_ "Tratamiento*Tendencia"
+label variable yerba_real "Precio del kilo de yerba a precios constantes"
+
+reg yerba_real treat t treat_t
+outreg2 using "$output/yerba_tabla.doc", nocons dec(2) label replace nonotes addnote("Errores estandar entre parentesis", "*** p<0.01, ** p<0.05, * p<0.1")
+
+*Sin estacionalidad:
+reg residuos treat t treat_t
+
+*Realizmaos otras estimaciones para Vestimenta y Calzado para chequear que los resultados son consistentes:
+extrdate month mes = Periodo 
+extrdate month year = Periodo
+
+reg yerba_real treat t treat_t i.mes
+
+reg yerba_real treat t treat_t, robust
+
+reg yerba_real treat t treat_t i.mes, robust
+
+*Permutaciones de inferencia (Heß, 2017): dado que el número de observaciones es relativamente pequeño, realizamos un análisis de inferencia aleatoria mediante permutaciones basadas en simulaciones de Monte Carlo a través del comando ritest.
+cap ssc install ritest
+
+ritest treat (_b[treat]/_se[treat]), reps(1000) seed(123) kdensityplot:reg yerba_real treat t treat_t
+
+
+ritest treat (_b[treat]/_se[treat]), reps(1000) seed(123) kdensityplot: reg residuos treat t treat_t
+
+*Además, reportaremos los errores estándar consistentes con heterocedasticidad y autocorrelación de Newey y West (1994).
+*El número óptimo de rezagos es el numero entero inferior de floor[4(30/100)^{2/9}]=3:
+tsset t
+unique t
+
+newey yerba_real treat t treat_t, lag(3)
+outreg2 using "$output/yerba_newey_tabla.doc", dec(2) label replace nonotes addnote("Errores estandar consistentes con heterocedasticidad y autocorrelacion de Newey y West entre parentesis", "*** p<0.01, ** p<0.05, * p<0.1")
+newey yerba_real treat t treat_t i.mes, lag(3)
+
+**** Estimaciones 2023-2024 ****
+import excel "$input/yerba_precio_est.xlsx", firstrow clear
+drop mes
+sort Periodo
+drop if Periodo<=mdy(12,1,2022)
+gen treat=0
+replace treat=1 if Periodo>mdy(12,1,2023)
+gen t=_n
+gen treat_t=treat*t
+label variable t "Tendencia"
+label variable treat "Tratamiento"
+label variable treat_ "Tratamiento*Tendencia"
+label variable yerba_real "Precio del kilo de yerba a precios constantes"
+extrdate month mes = Periodo 
+extrdate month year = Periodo
+
+reg yerba_real treat t treat_t i.mes
